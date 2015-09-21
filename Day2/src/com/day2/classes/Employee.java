a2986ad1c189e708b513378ff005cfb93ca2ae10
vishipayyallore <vishipayyallore@yahoo.com>
2015-09-21 08:34:37 +0530

    21-Sep-2015

---
+++ Day2/src/com/day2/classes/Employee.java
@@ -0,0 +1,24 @@
+package com.day2.classes;
+
+import org.bson.Document;
+
+/**
+ * 
+ * @author Viswanatha
+ *
+ */
+
+public class Employee extends Document {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public Employee(){
+	}
+	
+	public String Id;
+	public String Name;
+	public String Designation;
+}
